214e131edf9511dea4b8e1a42c6abfad062411e8
Kristianto <kristianto.pratama@ti.ukdw.ac.id>
2021-10-31 00:52:08 +0700

    Pertemuan 7

--- app/src/main/java/com/example/pertemuan7_71180283/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan7_71180283/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.pertemuan7_71180283
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val listSong = arrayListOf<Song>()
+        listSong.add(Song(1, "Sampai Kau Bicara", "Hijau Daun", R.mipmap.hijau_daun))
+        listSong.add(Song(2, "Pujaan Hati", "Kangen Band", R.mipmap.kangen_band))
+
+        val RcySong = findViewById<RecyclerView>(R.id.RcySong)
+        RcySong.layoutManager = LinearLayoutManager(this)
+        val adapter = SongAdapter(listSong, this)
+        RcySong.adapter = adapter
+
+
     }
 }
--- app/src/main/java/com/example/pertemuan7_71180283/SongAdapter.kt
+++ app/src/main/java/com/example/pertemuan7_71180283/SongAdapter.kt
@@ -1,17 +1,38 @@
 package com.example.pertemuan7_71180283
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
-class SongAdapter(var listSong: ArrayList<Song>): RecyclerView.Adapter<SongAdapter.SongHolder>() {
+class SongAdapter(var listSong: ArrayList<Song>, var context: Context): RecyclerView.Adapter<SongAdapter.SongHolder>() {
     class SongHolder(val view: View): RecyclerView.ViewHolder(view){
-        fun bind(song: Song){
+        fun bind(song: Song, context: Context){
             view.findViewById<ImageView>(R.id.Cover).setImageResource(song.Cover)
-            view.findViewById<TextView>(R.id.Judul).setText(song.Judul)
+            view.findViewById<TextView>(R.id.Judul).setText("#${song.rank} ${song.Judul}")
             view.findViewById<TextView>(R.id.Penyanyi).setText(song.Penyanyi)
+            view.setOnClickListener(){
+                Toast.makeText(context, "${song.Penyanyi}: ${song.Judul}", Toast.LENGTH_SHORT).show()
+            }
 
         }
+    }
+
+    override fun getItemCount(): Int {
+        return listSong.size
+
+    }
+
+    override fun onBindViewHolder(holder: SongHolder, position: Int) {
+        holder.bind(listSong[position], context)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SongHolder {
+       val a = LayoutInflater.from(parent.context).inflate(R.layout.item_song, parent, false)
+        return SongHolder(a)
     }
 }
