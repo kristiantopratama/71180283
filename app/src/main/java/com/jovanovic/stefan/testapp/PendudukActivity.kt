f5b0bc2a3338fc307a8217c8e2e5c6d52300368f
Kristianto <kristianto.pratama@ti.ukdw.ac.id>
2021-12-18 04:17:11 +0700

    Final-71180283

--- app/src/main/java/com/jovanovic/stefan/testapp/PendudukActivity.kt
+++ app/src/main/java/com/jovanovic/stefan/testapp/PendudukActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -28,13 +29,13 @@ class PendudukActivity: AppCompatActivity()  {
         btnSave.setOnClickListener{
             val penduduk = Penduduk(edtNama.text.toString(), edtUsia.text.toString(), edtAlamat.text.toString())
             firestore?.collection("Penduduk")?.document(penduduk.Nama)?.set(penduduk)
-
+            Toast.makeText(getApplicationContext(), "Saved",Toast.LENGTH_SHORT).show()
         }
 
         btnSearch.setOnClickListener{
             firestore?.collection("Penduduk")?.get()!!
                 .addOnSuccessListener { docs ->
-                    var output = " "
+                    var output = "Data "
                     for(doc in docs){
                         output += "\n${doc.data}"
                     }
